39726d42f6ad6a806d463af4adf6c79e29995c93
Victor Alves Santana <viclucigei.123@gmail.com>
2024-10-01 12:15:26 -0300

    add ternário no js

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ const menu = document.querySelector(".menu-lateral")
 const botao = document.querySelector(".btn")
 const content = document.querySelector(".content") 
 const background = document.querySelector(".background")
+const body = document.querySelector("body")
 
 function abrirMenu(){
     menu.classList.toggle('ativo')
     botao.classList.toggle('ativo')
     content.classList.toggle('ativo')
     background.classList.toggle('ativo')
+    body.style.backgroundColor = menu.classList.contains('ativo') ? '#34495e' : '#ecf0f1'
 }
